4e0b438b2c53a99a725cb56b1ee0b72cd74a8f01
sharath chandran <75419524+sharath-wq@users.noreply.github.com>
2024-01-15 12:30:18 +0530

    Update checkouthelper.js
    fixed the moduel not found error

--- helper/checkouthelper.js
+++ helper/checkouthelper.js
@@ -1,125 +1,115 @@
-const asynchandler = require("express-async-handler");
-const Cart = require("../models/cartModel");
-const Product = require("../models/productModel");
-const OrderItems = require("../models/orderItemModel");
-const User = require("../models/userModel");
-const Order = require("../models/orderModel");
-const Address = require("../models/addressModel");
-const { generateUniqueOrderID } = require("../utils/generateUniqueid");
+const asynchandler = require('express-async-handler');
+const Cart = require('../models/cartModel');
+const Product = require('../models/productModel');
+const OrderItems = require('../models/orderItemModel');
+const User = require('../models/userModel');
+const Order = require('../models/orderModel');
+const Address = require('../models/addressModel');
+const { generateUniqueOrderID } = require('../utils/generateUniqueId');
 const Crypto = require('crypto');
 const Wallet = require('../models/walletModel');
 const WalletTransaction = require('../models/walletTransactionModel');
-const Coupon = require('../models/couponModel')
+const Coupon = require('../models/couponModel');
 
 //Get user cart items
 exports.getCartItems = asynchandler(async (userId) => {
-    return await Cart.findOne({ user:userId}).populate("products.product");
-  });
+    return await Cart.findOne({ user: userId }).populate('products.product');
+});
 
+//Calculate the total price of cart items
 
+// exports.calculateTotalPrice = asynchandler(
+//   async (cartItems, userid, payWithWallet, coupon) => {
+//     const wallet = await Wallet.findOne({ user: userid });
+//     console.log(wallet,"Users wallet");
+//     let subtotal = 0;
+//     for (const product of cartItems.products) {
+//       const productTotal =
+//         parseFloat(product.product.salePrice) * product.quantity;
+//       subtotal += productTotal;
+//       console.log(productTotal,"productTotal");
+//     }
 
-  //Calculate the total price of cart items
-  
-  // exports.calculateTotalPrice = asynchandler(
-  //   async (cartItems, userid, payWithWallet, coupon) => {
-  //     const wallet = await Wallet.findOne({ user: userid });
-  //     console.log(wallet,"Users wallet");
-  //     let subtotal = 0;
-  //     for (const product of cartItems.products) {
-  //       const productTotal =
-  //         parseFloat(product.product.salePrice) * product.quantity;
-  //       subtotal += productTotal;
-  //       console.log(productTotal,"productTotal");
-  //     }
-  
-  //     let total;
-  //     let usedFromWallet = 0;
-  //     if (wallet && payWithWallet) {
-  //       let discount = 0;
-  //       total = subtotal;
-  
-  //       if (coupon) {
-  //         discount = ((total * coupon.value) / 100).toFixed(2);
-  //         if (discount > coupon.maxAmount) {
-  //           discount = coupon.maxAmount;
-  //           total -= discount;
-  //         } else {
-  //           total -= discount;
-  //         }
-  //       }
-  
-  //       if (total <= wallet.balance) {
-  //         usedFromWallet = total;
-  //         wallet.balance -= total;
-  //         total = 0;
-  //       } else {
-  //         usedFromWallet = wallet.balance;
-  //         total = subtotal - wallet.balance - discount;
-  //         wallet.balance = 0;
-  //       }
-  //       return {
-  //         subtotal,
-  //         total,
-  //         usedFromWallet,
-  //         walletBalance: wallet.balance,
-  //         discount: discount ? discount :0,
-  //       };
-  //     } else {
-  //       total = subtotal;
-  //       let discount = 0;
-  //       if (coupon) {
-  //         discount = ((total * coupon.value) / 100).toFixed(2);
-  //         if (discount > coupon.maxAmount) {
-  //           discount = coupon.maxAmount;
-  //           total -= discount;
-  //         } else {
-  //           total -= discount;
-  //         }
-  //       }
-  //       return {
-  //         subtotal,
-  //         total,
-  //         usedFromWallet,
-  //         walletBalance: wallet ? wallet.balance : 0,
-  //         discount: discount ? discount : 0,
-  //       };
-  //     }
-  //   }
-  // );
+//     let total;
+//     let usedFromWallet = 0;
+//     if (wallet && payWithWallet) {
+//       let discount = 0;
+//       total = subtotal;
 
+//       if (coupon) {
+//         discount = ((total * coupon.value) / 100).toFixed(2);
+//         if (discount > coupon.maxAmount) {
+//           discount = coupon.maxAmount;
+//           total -= discount;
+//         } else {
+//           total -= discount;
+//         }
+//       }
 
-  exports.calculateTotalPrice = asynchandler(
-    async (cartItems, userid, coupon) => {
+//       if (total <= wallet.balance) {
+//         usedFromWallet = total;
+//         wallet.balance -= total;
+//         total = 0;
+//       } else {
+//         usedFromWallet = wallet.balance;
+//         total = subtotal - wallet.balance - discount;
+//         wallet.balance = 0;
+//       }
+//       return {
+//         subtotal,
+//         total,
+//         usedFromWallet,
+//         walletBalance: wallet.balance,
+//         discount: discount ? discount :0,
+//       };
+//     } else {
+//       total = subtotal;
+//       let discount = 0;
+//       if (coupon) {
+//         discount = ((total * coupon.value) / 100).toFixed(2);
+//         if (discount > coupon.maxAmount) {
+//           discount = coupon.maxAmount;
+//           total -= discount;
+//         } else {
+//           total -= discount;
+//         }
+//       }
+//       return {
+//         subtotal,
+//         total,
+//         usedFromWallet,
+//         walletBalance: wallet ? wallet.balance : 0,
+//         discount: discount ? discount : 0,
+//       };
+//     }
+//   }
+// );
 
-      let subtotal = 0;
-      for (const product of cartItems.products) {
-        const productTotal =
-          parseFloat(product.product.salePrice) * product.quantity;
+exports.calculateTotalPrice = asynchandler(async (cartItems, userid, coupon) => {
+    let subtotal = 0;
+    for (const product of cartItems.products) {
+        const productTotal = parseFloat(product.product.salePrice) * product.quantity;
         subtotal += productTotal;
-      }
-  
-      let total;
-        total = subtotal;
-        let discount = 0;
-        if (coupon) {
-          discount = ((total * coupon.value) / 100).toFixed(2);
-          if (discount > coupon.maxAmount) {
+    }
+
+    let total;
+    total = subtotal;
+    let discount = 0;
+    if (coupon) {
+        discount = ((total * coupon.value) / 100).toFixed(2);
+        if (discount > coupon.maxAmount) {
             discount = coupon.maxAmount;
             total -= discount;
-          } else {
+        } else {
             total -= discount;
-          }
         }
-        return {
-          subtotal,
-          total,
-          discount: discount ? discount : 0,
-        };
-      
     }
-  );
-
-
+    return {
+        subtotal,
+        total,
+        discount: discount ? discount : 0,
+    };
+});
 
 //Place order
 
@@ -159,7 +149,7 @@ exports.getCartItems = asynchandler(async (userId) => {
 //         total = total - discount;
 //       }
 //     }
-   
+
 //     const address = await Address.findById(addressId);
 
 //     const existingOrderIds = await Order.find().select("orderId");
@@ -183,39 +173,37 @@ exports.getCartItems = asynchandler(async (userId) => {
 //   }
 // );
 
-exports.placeOrder = asynchandler(
-  async (userId, addressId, payment_method, code, isWallet, coupons) => {
+exports.placeOrder = asynchandler(async (userId, addressId, payment_method, code, isWallet, coupons) => {
     const cartItems = await exports.getCartItems(userId);
     const coupon = await Coupon.findOne({ code: code });
 
     if (!cartItems) {
-      throw new Error("cart not found");
+        throw new Error('cart not found');
     }
     const orders = [];
     let total = 0;
 
     for (const cartItem of cartItems.products) {
-      const productTotal =
-        parseFloat(cartItem.product.salePrice) * cartItem.quantity;
+        const productTotal = parseFloat(cartItem.product.salePrice) * cartItem.quantity;
 
-      total = total + productTotal;
+        total = total + productTotal;
 
-      const item = await OrderItems.create({
-        quantity: cartItem.quantity,
-        price: cartItem.product.salePrice,
-        product: cartItem.product._id,
-      });
-      orders.push(item);
+        const item = await OrderItems.create({
+            quantity: cartItem.quantity,
+            price: cartItem.product.salePrice,
+            product: cartItem.product._id,
+        });
+        orders.push(item);
     }
     let discount;
     if (coupon) {
-      discount = ((total * coupon.value) / 100).toFixed(2);
-      if (discount > coupon.maxAmount) {
-        discount = coupon.maxAmount;
-        total = total - discount;
-      } else {
-        total = total - discount;
-      }
+        discount = ((total * coupon.value) / 100).toFixed(2);
+        if (discount > coupon.maxAmount) {
+            discount = coupon.maxAmount;
+            total = total - discount;
+        } else {
+            total = total - discount;
+        }
     }
     // const updateProduct = await Product.findById(cartItem.product._id);
     // updateProduct.quantity -= cartItem.quantity;
@@ -224,49 +212,36 @@ exports.placeOrder = asynchandler(
 
     const address = await Address.findById(addressId);
 
-    const existingOrderIds = await Order.find().select("orderId");
+    const existingOrderIds = await Order.find().select('orderId');
     //Create the order
     const newOrder = await Order.create({
-      orderId: "OD" + generateUniqueOrderID(existingOrderIds),
-      user: userId,
-      orderItems: orders,
-      shippingAddress: address.name,
-      city: address.city,
-      street: address.street,
-      state: address.state,
-      zip: address.pincode,
-      phone: address.mobile,
-      totalPrice: total,
-      payment_method: payment_method,
-      coupon: coupon,
-      discount: discount,
+        orderId: 'OD' + generateUniqueOrderID(existingOrderIds),
+        user: userId,
+        orderItems: orders,
+        shippingAddress: address.name,
+        city: address.city,
+        street: address.street,
+        state: address.state,
+        zip: address.pincode,
+        phone: address.mobile,
+        totalPrice: total,
+        payment_method: payment_method,
+        coupon: coupon,
+        discount: discount,
     });
     return newOrder;
-  }
-);
+});
 
-  //varify using Razorpay method
+//varify using Razorpay method
 
-  exports.verifyPayment = asynchandler(
-    async (
-      razorpay_payment_id,
-      razorpay_order_id,
-      razorpay_signature,
-      orderId
-    ) => {
-      const sign = razorpay_order_id + "|" + razorpay_payment_id;
-      const expectedSign = Crypto.createHmac(
-        "sha256",
-        process.env.RAZORPAY_SECRET_KEY
-      )
-        .update(sign.toString())
-        .digest("hex");
-  
-      if (razorpay_signature === expectedSign) {
+exports.verifyPayment = asynchandler(async (razorpay_payment_id, razorpay_order_id, razorpay_signature, orderId) => {
+    const sign = razorpay_order_id + '|' + razorpay_payment_id;
+    const expectedSign = Crypto.createHmac('sha256', process.env.RAZORPAY_SECRET_KEY).update(sign.toString()).digest('hex');
+
+    if (razorpay_signature === expectedSign) {
         // console.log(true);
-        return { message: "success", orderId: orderId };
-      } else {
-        throw new Error("Payment verification failed");
-      }
+        return { message: 'success', orderId: orderId };
+    } else {
+        throw new Error('Payment verification failed');
     }
-  );
+});
